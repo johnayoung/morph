2490ca78f5047b596232436849074be4832a7025
John Young <john.young@spreadstreet.io>
2019-03-13 22:44:49 -0400

    finally got it. wasted day, but tomorrow is anew

--- client/src/api.js
+++ client/src/api.js
@@ -3,11 +3,11 @@ import { API_BASE_URL } from './config';
 
 const api = async (endpoint, input) => {
   const config = {
-    baseURL: `${API_BASE_URL}/strings/${endpoint}`,
-    method: 'post',
-    data: {
-      input
-    }
+    baseURL: `https://warm-peak-72707.herokuapp.com/api/endpoints/proxy`,
+    headers: {
+      'x-url-string': `https://morph.now.sh/${endpoint}`
+    },
+    method: 'get',
   }
   const response = await axios(config);
   return response;
